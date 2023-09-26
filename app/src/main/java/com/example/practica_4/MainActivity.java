7180ae21c181fd8babed9617b9e23099d4805bda
Arturo <hernandez020610@gmail.com>
2023-09-26 17:42:20 -0600

    Mejora de diseño y un poco de funcionalidad

--- app/src/main/java/com/example/practica_4/MainActivity.java
+++ app/src/main/java/com/example/practica_4/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.practica_4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+
+    Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(this, MainActivity2.class);
+        startActivity(i);
     }
 }
