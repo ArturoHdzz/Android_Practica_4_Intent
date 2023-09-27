e758f266f561ceef978f00a7e6fb317d744f5a30
Arturo <hernandez020610@gmail.com>
2023-09-27 17:35:43 -0600

    Mejora de diseño en letras y agregacion de botones

--- app/src/main/java/com/example/practica_4/MainActivity2.java
+++ app/src/main/java/com/example/practica_4/MainActivity2.java
@@ -11,6 +11,13 @@ import android.widget.Button;
 public class MainActivity2 extends AppCompatActivity implements View.OnClickListener{
 
     Button btn1;
+    Button btn2;
+    Button btn3;
+    Button btn4;
+    Button btn5;
+    Button btn6;
+    Button btn7;
+    Button btn8;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +25,27 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
 
         btn1 = findViewById(R.id.btn1);
         btn1.setOnClickListener(this);
+
+        btn2 = findViewById(R.id.btn2);
+        btn2.setOnClickListener(this);
+
+        btn3 = findViewById(R.id.btn3);
+        btn3.setOnClickListener(this);
+
+        btn4 = findViewById(R.id.btn4);
+        btn4.setOnClickListener(this);
+
+        btn5 = findViewById(R.id.btn5);
+        btn5.setOnClickListener(this);
+
+        btn6 = findViewById(R.id.btn6);
+        btn6.setOnClickListener(this);
+
+        btn7 = findViewById(R.id.btn7);
+        btn7.setOnClickListener(this);
+
+        btn8 = findViewById(R.id.btn8);
+        btn8.setOnClickListener(this);
     }
 
 
