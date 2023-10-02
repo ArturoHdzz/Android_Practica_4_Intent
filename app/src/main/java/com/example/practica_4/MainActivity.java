149f2c2a0294e049bcb45a0548f7336c042a6671
Arturo <hernandez020610@gmail.com>
2023-10-02 13:05:36 -0600

    Boton eliminado y se sustituyo por el Contador(CountDownTimer)

--- app/src/main/java/com/example/practica_4/MainActivity.java
+++ app/src/main/java/com/example/practica_4/MainActivity.java
@@ -6,23 +6,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.os.CountDownTimer;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
 
-    Button btn1;
+    TextView txt4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn1 = findViewById(R.id.btn1);
-        btn1.setOnClickListener(this);
+        txt4 = findViewById(R.id.txt4);
+
+        new CountDownTimer(5000, 1000) { // 5000 milisegundos = 5 segundos
+
+            public void onTick(long millisUntilFinished) {
+                txt4.setText("Segundos: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                Intent i = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(i);
+            }
+        }.start();
     }
 
     @Override
     public void onClick(View v) {
-        Intent i = new Intent(this, MainActivity2.class);
-        startActivity(i);
+
     }
 }
